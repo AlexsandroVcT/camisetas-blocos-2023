7a4196c5a76acfe3123e6d364c49f4bbb13e2785
Alexsandro VcT <alexsandro.vct@outlook.com>
2023-02-13 14:59:53 -0300

    Project 1.0

--- next.config.js
+++ next.config.js
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
+	images: {
+		domains: ['files.stripe.com'],
+	},
+	experimental: {
+		appDir: true,
+
+		experimental: {
+			images: {
+				allowFutureImage: true,
+			},
+		},
+	},
 }
 
 module.exports = nextConfig
---
+++ src/pages/api/checkout.ts
@@ -0,0 +1,34 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import { stripe } from './../../lib/stripe';
+
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { priceId } = req.body
+
+  if (req.method !== 'POST') {
+    return res.status(405).json({ error: 'Método não permitido.'})
+  }
+
+  if (!priceId) {
+    return res.status(400).json({ error: 'Preço não encontrado.'})
+  }
+
+  const successUrl = `${process.env.NEXT_URL}/success?session_id={CHECKOUT_SESSION_ID}`
+  const cancelUrl = `${process.env.NEXT_URL}/`
+  
+  const checkoutSession = await stripe.checkout.sessions.create({
+    success_url: successUrl,
+    cancel_url: cancelUrl,
+    mode: 'payment',
+    line_items: [
+      {
+        price: priceId,
+        quantity: 1,
+      }
+    ],
+  })
+
+  return res.status(201).json({
+    checkoutUrl: checkoutSession.url,
+  })
+}
